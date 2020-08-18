05b3658a88880f04ed85bf95d0041afa30ae0d17
John Heaven Le <jle14@huskers.unl.edu>
2020-08-18 02:01:41 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: John Heaven Le
+ * Date: August 16 2020
+ *
+ * A simple hello world program. Also my first program in C!
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello world!\n");
+
+    return 0;
+
+}
