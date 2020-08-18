e4b7418a0d20839c9f6506058f93875772566792
Theresa Mai <tmai7@huskers.unl.edu>
2020-08-18 03:45:28 +0000

    changed info to Theresa

--- hello.c
+++ hello.c
@@ -11,6 +11,8 @@ int main(int argc, char **argv) {
 
     printf("Helena Tran!\n");
     printf("Computer Science\n");
+    printf("Theresa Mai!\n");
+    printf("Computer Science\n");
 
     return 0;
 
